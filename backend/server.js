f77a8a43b4625f7c8099492d59f27487b1550198
rapolupraneeth <155894161+rapolupraneeth@users.noreply.github.com>
2026-06-20 17:14:26 +0530

    Set default port to 5000 if not specified

--- backend/server.js
+++ backend/server.js
@@ -25,8 +25,8 @@ app.use('/api/tasks',taskRoutes);
 mongoose.connect(process.env.MONGO_URI)
     .then (()=>{
         console.log('MongoDB Connected');
-        app.listen(process.env.PORT,()=>{
-            console.log(`Server running on port ${process.env.PORT}`);
+        app.listen(process.env.PORT || 5000,()=>{
+            console.log(`Server running on port ${process.env.PORT || 5000}`);
         });
     })
     .catch ((err)=>console.log('DB connection error:',err));
